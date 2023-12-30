94fe3150e222e587f673e8891aff71117b6402e8
Benjamin Gonzalez Jr. <bengonzalez2688+github@gmail.com>
2023-12-30 11:54:54 -0700

    functional prototype completed

--- projects/todolist/todolist.js
+++ projects/todolist/todolist.js
@@ -1,11 +1,11 @@
 // get reference to button and label element using their id
 var addItemButton = document.getElementById("addItemButton");
-var toDoListDiv = document.getElementById("toDoListDiv");
+var toDoListDiv = document.getElementById("toDoListDiv"); 
 
-function addItem() {
+function addItem() { 
     // create a new input-wrapper div
     var newInputWrapperDiv = document.createElement("div");
-    newInputWrapperDiv.setAttribute("class", "input-wrapper");  
+    newInputWrapperDiv.setAttribute("class", "input-wrapper");   
 
     // create a new input element
     var newInput = document.createElement("input");
@@ -26,6 +26,12 @@ function addItem() {
     var newCheckmarkDiv = document.createElement("div");
     newCheckmarkDiv.setAttribute("class", "checkmark");
 
+    // create a new deleteItem button
+    var newDeleteItemButton = document.createElement("button");
+    newDeleteItemButton.setAttribute("class","delete-item-button");
+    newDeleteItemButton.setAttribute("id","deleteItemButton");
+    newDeleteItemButton.innerHTML = "Delete Item";
+
     // append the newCheckedInput and newCheckmarkDiv to label
     newLabel.appendChild(newCheckedInput);
     newLabel.appendChild(newCheckmarkDiv);
@@ -36,8 +42,25 @@ function addItem() {
     // append the label element to the new input-wrapper div
     newInputWrapperDiv.appendChild(newLabel);
 
+    // append the deleteItem button to the new input-wrapper div
+    newInputWrapperDiv.appendChild(newDeleteItemButton);
+
     // append the new div to the target div
     toDoListDiv.appendChild(newInputWrapperDiv);
-} 
+}
 
-addItemButton.addEventListener("click", addItem);
+addItemButton.addEventListener("click", addItem); 
+
+function deleteItem(event) {
+    // get the parent div (input-wrapper)
+    var parentDiv = event.target.parentNode;
+
+    // remove the parent div from the DOM
+    parentDiv.remove();
+}
+
+toDoListDiv.addEventListener("click", function(event) {
+    if (event.target.classList.contains("delete-item-button")) {
+        deleteItem(event);
+    }
+});
